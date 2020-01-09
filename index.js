03e442f823ffc16003678808f4916f7004987514
Mikey Mann <mikey.mannx3@gmail.com>
2020-01-08 19:37:24 -0800

    Changed input to be user text input and sanitized the inputs to work with the api endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Retrieves the image from the DOG API Breed Endpoint based on user input.
+
 function getDogImage(dogInput) {
   fetch(`https://dog.ceo/api/breed/${dogInput}/images/random`)
     .then(response => response.json())
@@ -8,23 +10,26 @@ function getDogImage(dogInput) {
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+//Displays the breed photo to the DOM
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  $('.js-search-results').html(`<img src=" ${responseJson.message}">`);
-  
+  $('.js-search-results').html(`<img src="${responseJson.message}">`);
 }
 
+
+// This function looks at the input text box and sanitizes the input by removing whitespace and making
+// the string all lowercase.
 
   function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    let dogInput = $('#dropDownSelect').val();
+    let dogInput = $('#typed-breed').val().replace(/ +/g, "").toLowerCase();
     getDogImage(dogInput);
   });
 }
 
-
+//Initilize the App
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
